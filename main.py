3956d3e930437fa692f51d5a5bb083a3363b3849
ramanauksas <tomas_ramanauskas@yahoo.com>
2024-10-14 11:02:51 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+numbers = [1,5, 10]
+print(numbers)
+
+# 1 uzduotis
+names = ["Audrius", "Giedre", "Aiste", "Agne"]
+print(names[0], names[-1], len(names))
+
+# 2 uzduotis
+heights = [1.95, 1.80, 1.6, 1.45, 1.8, 2.0]
+print(f"heights: {heights}; length: {len(heights)}")
+
+# #  3 uzduotis
+# attributes_str = input("Iveskite pozymiu sarasa, atskirdami tarpais:")
+# attributes_list = attributes_str.split()
+# print(f"Pozymiai: {attributes_list}; kiekis: {len(attributes_list)}")
+
+
+# #  4 uzduotis
+# town_list = ["Vilnius", "Kaunas", "Klaipeda"]
+# initial_town_list = town_list[:]
+# input_towns_str = input("Iveskite nauju miestu atskirdami kableliu: ")
+# input_towns_list = input_towns_str.split(",")
+# town_list.extend(input_towns_list)
+# print(f"Pradinis miestu sarasas: {initial_town_list}\nPapildytas miestu sarasas: {town_list}")
+# insert_town = input("Iterpkite nauja miesta: ")
+# insert_position = int(input("Pasirinkite, kur iterpti: "))
+# town_list.insert(insert_position, insert_town)
+# print(f"Naujas miestu sarasas: {town_list}")
+
+
+# 5 uzduotis
+my_list = [1, 5, 10, ]
+
